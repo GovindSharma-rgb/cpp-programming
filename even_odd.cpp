9e81fd0287163cc9e7bd680d25e0c11ace6bfd97
Govind Sharma <govindshrama043136@gmail.com>
2026-02-09 23:43:09 +0530

    add my file

--- even_odd.cpp
+++ even_odd.cpp
@@ -1,1 +1,16 @@
-#include
+#include <iostream>
+
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n%2==0)
+    {
+        cout<<"Even Number";
+    }
+    else
+    {
+        cout<<"Odd Number"; 
+    }
+   
+}
